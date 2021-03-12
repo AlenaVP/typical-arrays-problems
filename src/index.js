d751da76c3a5ccf1a343fa4adb6530584be2771f
AlenaVP <poprugo@gmail.com>
2021-03-12 14:41:40 +0300

    feat: add a solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
+function validData (array) {
+  return (array === undefined) ? false : ((typeof array == Array) || (array.length > 0) ? true : false);
+}
 
 exports.min = function min (array) {
-  return 0;
+  return validData (array) ? array.reduce((m, a) => !isNaN(m) && !isNaN(a) ? Math.min(m, a) : 0) : 0;
 }
 
 exports.max = function max (array) {
-  return 0;
+  return validData (array) ? array.reduce((m, a) => !isNaN(m) && !isNaN(a) ? Math.max(m, a) : 0) : 0;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  return validData (array) ? array.reduce((s, a) => !isNaN(s) && !isNaN(a) ? s + a : 0) / array.length : 0;
 }
